4da94a1a33d1d7d1d4e1fe0406c4af983cee12cc
yugwangun <yugwangun@gmail.com>
2023-10-13 16:51:00 +0900

    fix: value type

--- 20231012/lecture1.c
+++ 20231012/lecture1.c
@@ -5,14 +5,14 @@ int main()
     int a = 3;
     int *ptr1 = &a;
 
-    char b = "b";
-    int *ptr2 = &b;
+    char b = 'b';
+    char *ptr2 = &b;
 
-    printf("%d\n", ptr1);
-    printf("%d\n", sizeof(ptr1));
+    printf("%p\n", ptr1);
+    printf("%lu\n", sizeof(ptr1));
 
-    printf("%d\n", ptr2);
-    printf("%d\n", sizeof(ptr2));
+    printf("%p\n", ptr2);
+    printf("%lu\n", sizeof(ptr2));
 
     return 0;
 }
